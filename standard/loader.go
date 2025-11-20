673421e61eb06196c021c018e595b4d9ff2c64b9
dywoq <aleks.koyf@gmail.com>
2025-11-20 14:49:06 +0300

    standard: add LoaderPrinter

--- standard/loader.go
+++ standard/loader.go
@@ -1,17 +1,20 @@
 package standard
 
-
 // LoaderInterruptions contains the methods that are standard and officially supported.
 // This interface should be implemented by all loaders to ensure compatibility with other methods
 // who use LoaderInterruptions.
 //
 // To implement the interface, you would need to realize it in your loader structure.
 //
+// All methods inside LoaderInterruptions must run only in the gathering mode,
+// otherwise, they return an error.
+//
 // WARNING: If you add these methods to your structure, make sure they're also available
 // through using Interrupt method.
 // You can find all gates in inline documentation of methods inside LoaderInterruptions.
 type LoaderInterruptions interface {
 	LoaderLeaver
+	LoaderPrinter
 }
 
 type LoaderLeaver interface {
@@ -20,5 +23,13 @@ type LoaderLeaver interface {
 	//
 	// Its gate and signature:
 	// 	0x00 ()
-	Leave()
+	Leave() error
+}
+
+type LoaderPrinter interface {
+	// Print prints the message.
+	//
+	// Its gate and signature:
+	//  0x01 (string)
+	Print(string) error
 }
