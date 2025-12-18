457044d62be6fb0e30eb1c4a6109b86da43aac5d
dywoq <aleks.koyf@gmail.com>
2025-12-18 12:31:37 +0300

    crates/boot: Add Info structure to combine all in one

--- crates/boot/src/lib.rs
+++ crates/boot/src/lib.rs
@@ -4,3 +4,12 @@
 pub mod acpi;
 pub mod graphics;
 pub mod time;
+
+/// Boot loader information.
+#[repr(C)]
+pub struct Info {
+    framebuffer: graphics::FrameBuffer,
+    acpi: acpi::Acpi,
+    snapshot_time: time::SnapshotTime,
+}
+
